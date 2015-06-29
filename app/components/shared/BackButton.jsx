a3a08b70f523b286f637394688326e03f690bb14
miyohide <miyohide@gmail.com>
2015-06-29 23:18:06 +0900

    Add BackButton

---
+++ app/components/shared/BackButton.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import BackButton from "reapp-ui/components/buttons/BackButton";
+
+export default (
+    <BackButton onTap={() => window.history.back() } stopPropagation>
+        Back
+    </BackButton>
+);
+
